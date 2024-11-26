d88d5be8ee7e89ff7cc6a6a70d5d7aa7c41270c4
Nil Borodulia <brdnlsrg@gmail.com>
2024-11-26 11:40:06 +0100

    Add DockerCompose start and stop methods

--- app/main.go
+++ app/main.go
@@ -6,21 +6,52 @@ import (
 	"os/exec"
 )
 
+type DockerCompose struct {
+	Path   string
+	Status string
+}
+
+func (d DockerCompose) String() string {
+	return fmt.Sprintf("Path: %s, Status: %s", d.Path, d.Status)
+}
+
+func (d DockerCompose) Start() {
+	_, err := exec.Command("docker-compose", "up", "-d").Output()
+	if err != nil {
+		fmt.Println("Docker compose up failed")
+	}
+}
+
+func (d DockerCompose) Stop() {
+	_, err := exec.Command("docker-compose", "down").Output()
+	if err != nil {
+		fmt.Println("Docker compose down failed")
+	}
+}
+
 func main() {
 	dockerPaths := []string{
 		"/path/to/docker-compose-1",
 	}
 
+	composes := []DockerCompose{}
+
 	for _, path := range dockerPaths {
+		composes = append(composes, DockerCompose{Path: path, Status: "stopped"})
+	}
+
+	for index, compose := range composes {
 		// Run docker-compose ps
-		os.Chdir(path)
-		output, err := exec.Command("docker-compose", "ps").Output()
+		os.Chdir(compose.Path)
+		_, err := exec.Command("docker-compose", "ps").Output()
 		if err != nil {
 			fmt.Println("Docker compose ps failed")
 			return
 		}
+		composes[index].Status = "running"
 
-		fmt.Println(string(output))
-
+		//fmt.Println(string(output))
 	}
+
+	fmt.Println(composes)
 }
